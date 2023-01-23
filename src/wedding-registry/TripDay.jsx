2b2f0201788235cc69bf6386c6f43ed4993e38f3
Paty Cuenca <patycuenca@gmail.com>
2023-01-23 10:39:08 +0100

    removed old static html/css

--- src/wedding-registry/TripDay.jsx
+++ src/wedding-registry/TripDay.jsx
@@ -1,10 +1,17 @@
 const TripDay = ({ dayNumber, dayNumberClicked, dayTripDetails }) => {
   if (dayNumber == dayNumberClicked) {
-    console.log("dayTrip details: ", dayTripDetails);
+    const dayItems = dayTripDetails.dayItems;
+
     return (
-      <div>
-        <p>Trip Day Number {dayNumber + 1}</p>
-        <p>{dayTripDetails.dayTitle}</p>
+      <div className="mx-auto mb-5 w-4/5">
+        <b>{dayTripDetails.calendarDay}</b>
+        <div className="border border-solid border-slate-300 bg-slate-200">
+          {dayItems.map((item) => (
+            <p key={item.itemTitle}>
+              {item.itemTitle} - {item.itemValue}
+            </p>
+          ))}
+        </div>
       </div>
     );
   } else return null;
