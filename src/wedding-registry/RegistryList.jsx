8579ffcb95730f0cd4dfa1a45f2e9080e02d34db
Paty Cuenca <patycuenca@gmail.com>
2023-01-28 20:17:36 +0100

    cleanup

--- src/wedding-registry/RegistryList.jsx
+++ src/wedding-registry/RegistryList.jsx
@@ -1,41 +1,10 @@
 import GiftList from "./GiftList";
 
 const RegistryList = () => {
-  /*
   return (
-    <div className="bg-white/85 my-0 mx-auto flex w-11/12 flex-row flex-wrap">
-      <div className="m-10 basis-full justify-center border-b-2 p-0 text-center text-gold ">
-        <h2 className="pb-5 text-center text-2xl md:my-0 md:mx-auto">
-          La nostra lluna de mel... a Nova Zelanda 💍
-        </h2>
-      </div>
-      <div className="mt-1 basis-full md:basis-1/3">
-        <img
-          src="../img/newzealand-map.png"
-          alt="map of New Zealand with pins on it to signal de stops in a trip"
-          className="rounded-lg"
-        />
-      </div>
-      <div className="mx-auto sm:basis-full md:-mt-5 md:basis-2/3">
-        <Trip />
-      </div>
-    </div>
-  );
-
-
-  <picture>
-    <img
-      srcSet="
-      ../img/new_zealand_cover_w_300.jpg 300w,
-      ../img/new_zealand_cover_w_1315.jpg 1315w,
-      ../img/new_zealand_cover_w_1987.jpg 1987w,
-      ../img/ new_zealand_cover_w_2400.jpg 2400w"
-      src="../img/new_zealand_cover_w_2400.jpg"
-      alt="Snowed mountains and a lake in a New Zealander landscape"
-      className="w-full"
-    />
-  </picture>
-
+    <div className="w-ful lp-0 m-0">
+      <div className="m-0 h-full w-full p-0">
+        <div className="relative w-full">
           <picture className="max-h-fit ">
             <source
               srcSet="../img/ new_zealand_cover_w_2400.jpg 2400w"
@@ -55,18 +24,6 @@ const RegistryList = () => {
               className="w-full"
             />
           </picture>
-
-  */
-
-  return (
-    <div className="w-ful lp-0 m-0">
-      <div className="m-0 h-full w-full p-0">
-        <div className="relative w-full">
-          <img
-            src="../img/new_zealand_cover_w_2400.jpg"
-            alt="Snowed mountains and a lake in a New Zealander landscape"
-            className="w-full"
-          />
           <div className="absolute top-1/4 left-4">
             <h1 className="text-3xl md:text-7xl">Nova Zelanda 💍</h1>
             <h2 className="md:text-xl">
