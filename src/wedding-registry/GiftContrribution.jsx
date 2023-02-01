5b969593ff25b273616e3ed1123cd96ed346d5cd
Paty Cuenca <patycuenca@gmail.com>
2023-02-01 20:17:23 +0100

    added data validation to enable/disable the contribution button

--- src/wedding-registry/GiftContrribution.jsx
+++ src/wedding-registry/GiftContrribution.jsx
@@ -1,13 +1,13 @@
 const GiftContribution = ({ handleAccepted }) => (
   <div className="mt-6">
     Accepted Contribution
-    <form
-      onSubmit={handleAccepted}
+    <button
+      className="rounded-lg bg-black p-2 text-white hover:bg-gold"
+      type="button"
+      onClick={handleAccepted}
     >
-      <button className="rounded-lg bg-black p-2 text-white hover:bg-gold">
-        Cancel
-      </button>
-    </form>
+      Cancel
+    </button>
   </div>
 );
 
--- src/wedding-registry/GiftDetails.jsx
+++ src/wedding-registry/GiftDetails.jsx
@@ -1,23 +1,30 @@
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
 import ProgressBar from "../utils/ProgressBar";
 import GiftContribution from "./GiftContrribution";
 
 const GiftDetails = ({ id, gift }) => {
   const [acceptedContribution, setAcceptedContribution] = useState(false);
-  
-  const navigate = useNavigate();
-
+  const [invalidContribution, setInvalidContribution] = useState(true);
   const amountToFund = parseInt(gift.itemValue) - parseInt(gift.itemFunded);
   const percentage = (
     (100 * parseInt(gift.itemFunded)) /
     parseInt(gift.itemValue)
   ).toFixed(0);
-   let allowToContribute = percentage < 100 ? true : false;
+  let allowToContribute = percentage < 100 ? true : false;
 
   const handleAcceptedContribution = () => {
-    setAcceptedContribution(false)
-  }
+    setAcceptedContribution(false);
+    setInvalidContribution(true);
+  };
+
+  const handleContributeInputValidation = (e) => {
+    console.log(e.target.value);
+    if (parseInt(e.target.value) > 0) {
+      setInvalidContribution(false);
+    } else {
+      setInvalidContribution(true);
+    }
+  };
 
   return (
     <div>
@@ -35,10 +42,16 @@ const GiftDetails = ({ id, gift }) => {
       {!acceptedContribution && allowToContribute && (
         <div className="mt-1 pt-3">
           <form
-            onSubmit={() => {
-              //e.preventDefault();
-              navigate("#");
-              setAcceptedContribution(true);
+            onSubmit={(e) => {
+              e.preventDefault();
+              const formData = new FormData(e.target);
+              const contributeValue = formData.get("contribute") ?? "";
+              if (parseInt(contributeValue) > 0) {
+                setAcceptedContribution(true);
+                console.log(contributeValue);
+              } else {
+                console.log("Valor = 0");
+              }
             }}
           >
             <div className="flex flex-row flex-wrap pt-4">
@@ -53,11 +66,16 @@ const GiftDetails = ({ id, gift }) => {
                 name="contribute"
                 placeholder="0"
                 className="h-10 max-w-[60px] flex-grow basis-3/5 rounded-l-lg border-l border-t border-b border-r-0 border-zinc-500 p-0 text-right"
+                onChange={(e) => handleContributeInputValidation(e)}
               />
               <span className="h-10 border-t border-b border-zinc-500 px-2 pt-2 text-center text-zinc-500">
                 €
               </span>
-              <button className="h-10  max-w-[120px] basis-2/5 rounded-r-lg border border-zinc-500 bg-gray p-2 font-bold text-zinc-500 hover:bg-gold">
+              <button
+                type="submit"
+                disabled={invalidContribution}
+                className="h-10  max-w-[120px] basis-2/5 rounded-r-lg border border-zinc-500 bg-gray p-2 font-bold text-zinc-500 hover:bg-gold disabled:bg-zinc-300 disabled:text-zinc-200"
+              >
                 Contribueix!
               </button>
             </div>
@@ -65,7 +83,7 @@ const GiftDetails = ({ id, gift }) => {
         </div>
       )}
       {acceptedContribution && (
-        <GiftContribution id={id} handleAccepted={handleAcceptedContribution}/>
+        <GiftContribution id={id} handleAccepted={handleAcceptedContribution} />
       )}
     </div>
   );
