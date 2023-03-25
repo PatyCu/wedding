10d74e0b8413a462e6047e7372a9f9eaf01af595
Paty Cuenca <patycuenca@gmail.com>
2023-03-25 19:16:33 +0100

    refactored server to use env

--- wedding_backend/src/server.js
+++ wedding_backend/src/server.js
@@ -1,33 +1,38 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
 const helmet = require("helmet");
-const cors = require('cors');
+const cors = require("cors");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+//const mongoString = "mongodb+srv://patycuenca:hBVP1ybxZDwBhU7J@cluster0.dl3atgi.mongodb.net/?retryWrites=true&w=majority";
+const mongoString = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@mcluster0.dl3atgi.mongodb.net/WeddingRegistry?retryWrites=true&w=majority`
+
+const PORT = process.env.PORT || 5005;
 
 const app = express();
-const port = process.env.PORT || 8000;
-app.use(helmet())
-//app.use(cors());
-//To-Do replace the domain.dom example value with my own domain
+
+mongoose.connect(mongoString);
+
+mongoose.connection.on("error", function(error) {
+  if (process.env.NODE_ENV === "development") {
+    console.log(error);
+  }
+});
+
+mongoose.connection.on("open", function() {
+  console.log("Connected to MongoDB database.");
+});
+
+app.use(helmet());
+
 app.use(cors({
   origin: process.env.NODE_ENV === "development" ? "*" : /domain\.com$/
-}))
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
+}));
 
-app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-//mongoose.connect('mongodb://localhost/my-database', {
-mongoose.connect('mongodb+srv://patycuenca:hBVP1ybxZDwBhU7J@cluster0.dl3atgi.mongodb.net/?retryWrites=true&w=majority', {    
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-});
-
-const connection = mongoose.connection;
-connection.once('open', () => {
-    console.log('MongoDB database connection established successfully');
-});
-
-app.listen(port, () => {
-    console.log(`Server is running on port: ${port}`);
-});
+app.listen(PORT, function () {
+  console.log(`Express app listening on port ${PORT}`);
+})
