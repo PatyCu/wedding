875f7773cc11fda1a6aa72312a4f65cc1b078b64
Paty Cuenca <patycuenca@gmail.com>
2023-04-26 15:17:46 +0200

    typeform fills the viewport

--- src/HomePage/Assistencia.jsx
+++ src/HomePage/Assistencia.jsx
@@ -2,11 +2,11 @@ import { Widget } from "@typeform/embed-react";
 
 const Assistencia = () => {
   return (
-    <div classname="w-full bg-red-400 h-screen">
+    <div classname=" bg-red-400">
       <Widget
         id="htdKRM0E"
         style={{ width: "100%", autoResize: "true" }}
-        className="h-[75vh]"
+        className="h-[75vh] md:h-[80vh] lg:h-[85vh]"
       />
     </div>
   );
