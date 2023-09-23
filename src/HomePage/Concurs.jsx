bb15d76aa8d9a04e2b592559ae7a4415e351350a
Paty Cuenca <patycuenca@gmail.com>
2023-09-23 21:54:51 +0200

    added some styling to Concurs

--- src/HomePage/Concurs.jsx
+++ src/HomePage/Concurs.jsx
@@ -3,8 +3,11 @@ import imgQRCode from "../img/icon-qrcode.jpg";
 
 const Concurs = () => {
   return (
-    <div id="container-concurs" className="my-0 mx-auto max-w-[1200px] p-10">
-      <h2 className="pb-5 text-center text-2xl text-gold md:my-0 md:mx-auto">
+    <div
+      id="container-concurs"
+      className="my-0 mx-auto max-w-[600px] p-10 text-center"
+    >
+      <h2 className="pb-5 text-2xl text-gold md:my-0 md:mx-auto">
         Concurs de fotografia!
       </h2>
       <p>
@@ -13,36 +16,36 @@ const Concurs = () => {
         sorpresa!
       </p>
       <div className="mt-4">
-        <h3 className="pb-5 text-left text-xl text-gold md:my-0 md:mx-auto">
+        <h3 className="pb-5 text-xl text-gold md:my-0 md:mx-auto">
           Què cal fer?
         </h3>
         <ul>
-          <li>
-            <div>
-              <p>
-                <b>Instal·lar l&apos;app WedShoots al teu mòbil.</b>
-              </p>
-              <p>
-                <img
-                  src={imgWedshoots}
-                  className="mx-auto w-24"
-                  alt="WedShoots app icon"
-                />
-              </p>
-              <p>
-                Descarrega l&apos;aplicació de{" "}
-                <a href="http://www.wedshoots.com">WedShoots</a> o busca-la a la
-                teva botiga d&apos;aplicacions.
-              </p>
-            </div>
+          <li className="mt-4 border-2 border-solid border-lightgold p-2">
+            <p>
+              <b>Instal·lar l&apos;app WedShoots al teu mòbil.</b>
+            </p>
+            <p className="mt-4">
+              <img
+                src={imgWedshoots}
+                className="mx-auto w-24"
+                alt="WedShoots app icon"
+              />
+            </p>
+            <p className="mt-4">
+              Descarrega l&apos;aplicació de{" "}
+              <a href="http://www.wedshoots.com">WedShoots</a> o busca-la a la
+              teva botiga d&apos;aplicacions.
+            </p>
           </li>
-          <li className="mt-4">
+          <li className="mt-4 border-2 border-solid border-lightgold p-2">
             <div>
               <p>
                 <b>Uneix-te al nostre àlbum</b>
               </p>
-              <p>Obre Wedshoots, posa el teu nom i escaneja aquest codi QR</p>
-              <img src={imgQRCode} className="mx-auto w-24" alt="QR Code" />
+              <p className="mt-4">
+                Obre Wedshoots, posa el teu nom i escaneja aquest codi QR
+              </p>
+              <img src={imgQRCode} className="mx-auto w-52" alt="QR Code" />
               <p>
                 També pots entrar a l&apos;app i buscar l&apos;àlbum amb el codi{" "}
               </p>
@@ -51,10 +54,12 @@ const Concurs = () => {
               </p>
             </div>
           </li>
-          <li className="mt-4">
-            Fes fotos durant el dia de la boda i pujar-les a l&apos;àlbum.
+          <li className="mt-4 border-2 border-solid border-lightgold p-2">
+            <p>
+              Fes fotos durant el dia de la boda i pujar-les a l&apos;àlbum.
+            </p>
+            <p>Espera a veure si ets un dels guanyadors!</p>
           </li>
-          <li>Espera a veure si ets un dels guanyadors!</li>
         </ul>
       </div>
     </div>
